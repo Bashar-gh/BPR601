5059a9a23307fd7bc2b5fa77fd5ec87265fb87b1
Bashar Al-Ghada <bashar.alghada13@gmail.com>
2024-04-04 18:58:58 +0300

    added reviews end points for admin

--- src/api/reviews/reviews.controller.ts
+++ src/api/reviews/reviews.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Req } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewListItem } from './models/types/review_list_item.type';
 import { CreateReviewDTO } from './models/dtos/create_review.dto';
@@ -30,5 +30,17 @@ export class ReviewsController {
     async reviewOrder(@Param('id') id: string, @Body() dto: CreateReviewDTO, @Req() request: any): Promise<StatusDTO> {
         let payload: JWT_Data = request.payload;
         return this.reviewService.create(payload.userId, { sideOrderId: id }, dto);
+    }
+    @Get("user/:id")
+    async getListByUser(@Param('id') userId: string): Promise<ArrayReturn<ReviewListItem>> {
+        return this.reviewService.getListByUser(userId);
+    }
+    @Delete(":id")
+    async deleteReview(@Param('id') id: string): Promise<StatusDTO> {
+        return this.reviewService.deleteReview(id);
+    }
+    @Put(":id")
+    async editReview(@Param('id') id: string, @Body() dto: CreateReviewDTO): Promise<StatusDTO> {
+        return this.reviewService.editReview(id, dto);
     }
 }
--- src/api/reviews/reviews.service.ts
+++ src/api/reviews/reviews.service.ts
@@ -8,6 +8,7 @@ import { StatusDTO } from 'src/global/models/dtos/status.dto';
 import { CreateReviewDTO } from './models/dtos/create_review.dto';
 import { ReservableService } from '../reservable/reservable.service';
 import { SideordersService } from '../sideorders/sideorders.service';
+import { ArrayReturn } from 'src/global/models/dtos/return_type.dto';
 type ReveiwFor = {
     serviceId?: string,
     sideOrderId?: string,
@@ -28,6 +29,47 @@ export class ReviewsService {
         if (!saved) {
             return { Status: false };
         }
+        let status = await this.updateAvrage(reviewFor);
+        return { Status: status };
+    }
+    async getServiceReviews(serviceId: string): Promise<ReviewListItem[]> {
+        let query = this.reviewModel.find({ serviceId: serviceId.toObjectID() });
+        query.populate("userId");
+        let data = await query.exec();
+        return data.map(mapReviewListItem);
+    }
+    async getSideOrderReviews(sideOrderId: string): Promise<ReviewListItem[]> {
+        let query = this.reviewModel.find({ sideOrderId: sideOrderId.toObjectID() });
+        query.populate("userId");
+        let data = await query.exec();
+        return data.map(mapReviewListItem);
+    }
+    async getListByUser(userId: string): Promise<ArrayReturn<ReviewListItem>> {
+        let data = await this.reviewModel.find({ userId: userId.toObjectID() }).exec();
+        return {
+            ARRAY: data.map(mapReviewListItem),
+        };
+    }
+    async deleteReview(id: string): Promise<StatusDTO> {
+        await this.reviewModel.findByIdAndDelete(id);
+        return {
+            Status: true,
+        };
+    }
+    async editReview(id: string, dto: CreateReviewDTO): Promise<StatusDTO> {
+        let result = await this.reviewModel.findByIdAndUpdate(id, {
+            rating: dto.rating,
+            reviewText: dto.reviewText,
+        }, { new: true });
+        let status = await this.updateAvrage({
+            serviceId: result?.serviceId.toString(),
+            sideOrderId: result?.sideOrderId.toString(),
+        });
+        return {
+            Status: status,
+        };
+    }
+    private async updateAvrage(reviewFor: ReveiwFor): Promise<boolean> {
         let allRelatedReviews = await this.reviewModel.find({
             serviceId: reviewFor.serviceId?.toObjectID(),
             sideOrderId: reviewFor.sideOrderId?.toObjectID(),
@@ -43,20 +85,7 @@ export class ReviewsService {
         if (reviewFor.sideOrderId) {
             await this.sideOrdersService.updateReviewSum(reviewFor.sideOrderId, total / allRelatedReviews.length, allRelatedReviews.length);
         }
-       
-        return { Status: true };
-    }
-    async getServiceReviews(serviceId: string): Promise<ReviewListItem[]> {
-        let query = this.reviewModel.find({ serviceId: serviceId.toObjectID() });
-        query.populate("userId");
-        let data = await query.exec();
-        return data.map(mapReviewListItem);
-    }
-    async getSideOrderReviews(sideOrderId: string): Promise<ReviewListItem[]> {
-        let query = this.reviewModel.find({ sideOrderId: sideOrderId.toObjectID() });
-        query.populate("userId");
-        let data = await query.exec();
-        return data.map(mapReviewListItem);
+        return true;
     }
 
 }
